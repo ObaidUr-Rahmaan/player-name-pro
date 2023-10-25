1488370466b7c272631abcf34050414f4f27d3db
Obaid Ur-Rahmaan <ourrahmaan@gmail.com>
2023-10-25 19:39:28 +0100

    Finish basic UI

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
   return (
     <div>
-      <h1>Dashboard</h1>
-      <UserButton afterSignOutUrl="/" />
+      <div className="flex justify-around p-6">
+        <h1 className="text-4xl">Dashboard</h1>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+
+      {/* Show Names generated and a Click me button*/}
+      <div className="flex flex-col items-center space-y-40">
+        <div className="mt-32">Generated Names</div>
+        <Button className="rounded-2xl bg-orange-700 text-white hover:bg-orange-900">
+          Generate Unique Player Name
+        </Button>
+      </div>
     </div>
   );
 };
