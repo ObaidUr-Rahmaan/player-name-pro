92cd50a8c4a7b0d358fea79a6c5c16303f9e59a6
Obaid Ur-Rahmaan <ourrahmaan@gmail.com>
2023-10-26 19:31:24 +0100

    Add user authentication redirect on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const user = useUser(); // <-- Use the hook to get the user's authentication status
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user.isSignedIn) {
+      router.push("/dashboard");
+    }
+  }, [router, user]);
+
   return (
     <div className="min-h-screen bg-gradient-to-r from-orange-100 to-red-100">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
